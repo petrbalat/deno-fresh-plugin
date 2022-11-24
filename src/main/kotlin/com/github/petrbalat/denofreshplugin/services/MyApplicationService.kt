aa636799c9375f1e0020caed448d5ff73444b8a8
GitHub Action <action@github.com>
2022-11-24 08:41:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/petrbalat/denofreshplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/petrbalat/denofreshplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.petrbalat.denofreshplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.petrbalat.denofreshplugin.MyBundle
 
 class MyApplicationService {
 
